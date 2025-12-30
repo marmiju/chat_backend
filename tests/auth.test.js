055ba5edd83cc27535f042be52f866d3ee345e14
MAR_mju <developerazizar@gmail.com>
2025-12-30 23:51:47 +0600

    con: jest and ci/cd

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,8 +1,25 @@
 import request from 'supertest'
 import { server } from '../server'
+import mongoose from 'mongoose'
 
 describe('Auth API', () => {
-  
+
+  beforeAll(async () => {
+    // Wait until DB is connected
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+    }
+  })
+
+  afterAll(async () => {
+    // Close server & DB connections
+    await mongoose.connection.close()
+    server.close()
+  })
+
   it('should register a user', async () => {
     const res = await request(server)
       .post('/api/user/register')
