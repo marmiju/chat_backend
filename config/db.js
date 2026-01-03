a415ebc1b4f13f0972b6b899297dd317b9c4a949
mar_miju <mar.miju.dev@gmail.com>
2026-01-03 21:24:37 +0600

    add mongo in docker

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
-import { MONGO_PASS, MONGO_USER } from "./env.js";
+import { MONGO_URI } from "./env.js";
+
+// ===========
 
 
-const username = encodeURIComponent(MONGO_USER);
-const pass = encodeURIComponent(MONGO_PASS);
-const dbname = "chatApp";
-
-const mongoString = `mongodb+srv://${username}:${pass}@cluster0.smwmg.mongodb.net/${dbname}?appName=Cluster0`;
-console.log(mongoString)
-
-export const connectDb = () => {
-  mongoose.connect(mongoString)
-    .then(() => console.log("✅ MongoDB connected"))
-    .catch((err) => console.error("❌ MongoDB connection failed", err));
+export const connectDb = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log(`✅ MongoDB connected `);
+  } catch (err) {
+    console.error("❌ MongoDB connection failed", err.message);
+    process.exit(1);
+  }
 };
--- config/env.js
+++ config/env.js
@@ -1,7 +1,14 @@
-
 import { configDotenv } from "dotenv";
-
 configDotenv();
+
+
+
+// mongo connection
 export const MONGO_USER = process.env.MONGO_USER
 export const MONGO_PASS = process.env.MONGO_PASS
+
+export const MONGO_URI = process.env.MONGO_URI || `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.smwmg.mongodb.net/chatApp`
+
+
+// jwt secret
 export const JWT_SECRET = process.env.JWT_SECRET
